33cdb4032183e03836a0bb9412f43713f1005856
luicasas <62635244+luicasas@users.noreply.github.com>
2021-10-05 02:48:12 -0500

    Add files via upload

---
+++ UVa1124.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UVa1124 {
+    public static void main(String[] args) throws Throwable {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String s;
+
+        while ((s = in.readLine()) != null) {
+            System.out.println(s);
+        }
+        in.close();
+    }
+}
