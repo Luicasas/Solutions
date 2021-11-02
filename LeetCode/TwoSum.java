70b71a9f331a43e66285224897802a830425a14d
Lucas Patron Rodriguez <62635244+Luicasas@users.noreply.github.com>
2021-11-02 14:59:05 -0500

    Update and rename Main.java to TwoSum.java

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,12 +1,4 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        int target = 6;
-        int[] arr = {3, 2, 3};
-        System.out.println(Arrays.toString(twoSum(arr, target)));
-    }
+class Solution {
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
