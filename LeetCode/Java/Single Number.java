5d1e75558801ea33dd6437fb154ab123cfeb4fbe
Lucas Patron Rodriguez <62635244+Luicasas@users.noreply.github.com>
2021-11-12 21:11:54 -0500

    Update Single Number.java

--- LeetCode/Java/Single Number.java
+++ LeetCode/Java/Single Number.java
@@ -1,4 +1,4 @@
-// Brute Force Solution:
+// Brute Force Solution (slow af)
 class Solution {
     public int singleNumber(int[] nums) {
         int result;
@@ -19,5 +19,46 @@ class Solution {
             if (result != Integer.MAX_VALUE) return result;
         }
         return Integer.MAX_VALUE;
+    }
+}
+
+// Sorted Array
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                return nums[i - 1];
+            } else if (i == nums.length - 2) {
+                return nums[nums.length - 1];
+            } else {
+                i += 1;
+            }
+        }
+        return 0;
+    }
+}
+
+// Elimination process (slow af)
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (temp == nums[j]) {
+                    nums[j] = Integer.MAX_VALUE;
+                    nums[i] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        for (int number : nums) {
+            if (number != Integer.MAX_VALUE) {
+                return number;
+            }
+        }
+        return -1;
     }
 }
