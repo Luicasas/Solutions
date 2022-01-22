65b36fca977f5bcf3dea4b02fe6c91d916ba2cb5
Lucas Patron Rodriguez <62635244+Luicasas@users.noreply.github.com>
2022-01-22 16:00:05 -0500

    Update Isograms.java

--- CodeWars/Isograms.java
+++ CodeWars/Isograms.java
@@ -2,10 +2,13 @@ class Solution {
     public boolean isIsogram(String str) {
         LinkedHashSet<Character> set = new LinkedHashSet<>();
         char[] chars = str.toLowerCase().toCharArray();
+        
         for (char character : chars) {
             set.add(character);
         }
+        
         String result = set.toString();
+        
         return result.equals(Arrays.toString(chars));
     }
 }
