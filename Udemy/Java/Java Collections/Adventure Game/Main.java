943f3e8d643d64ae5ce59da7db4f583eff4c1156
Lucas Patron Rodriguez <62635244+Luicasas@users.noreply.github.com>
2022-01-26 23:38:19 -0500

    Update Main.java

--- Udemy/Java/Java Collections/Adventure Game/Main.java
+++ Udemy/Java/Java Collections/Adventure Game/Main.java
@@ -1,36 +1,40 @@
 import java.io.*;
-import java.lang.reflect.AnnotatedArrayType;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    private static Map<Integer, Location> locations = new HashMap<>();
+    private static final Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in the forest"));
-        locations.put(5, new Location(5, "You are in Colombia."));
+        HashMap<String, Integer> tempMap = new HashMap<>();
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning java", tempMap));
 
-        locations.get(2).addExit("N", 5);
-
-        locations.get(3).addExit("W", 1);
-
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-
+        // Location 1
+        tempMap.put("W", 2);
+        tempMap.put("E", 3);
+        tempMap.put("S", 4);
+        tempMap.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick", tempMap));
+        // Location 2
+        tempMap = new HashMap<>();
+        tempMap.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempMap));
+        // Location 3
+        tempMap = new HashMap<>();
+        tempMap.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempMap));
+        // Location 4
+        tempMap = new HashMap<>();
+        tempMap.put("N", 1);
+        tempMap.put("W", 2);
+        locations.put(4, new Location(4, "You are in the forest", tempMap));
+        // Location 5
+        tempMap = new HashMap<>();
+        tempMap.put("S", 1);
+        tempMap.put("W", 2);
+        locations.put(5, new Location(5, "You are in Colombia.", tempMap));
         int location = 1;
         while (true) {
             System.out.println(locations.get(location).getDescription());
